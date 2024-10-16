aefd8179b2e11b058e817145ff2c0bb72aea70e7
personal <hassanaly@gmail.com>
2024-10-16 10:12:26 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        String apiKey = "this-is-a-fake-api-key-12345";
+        String dbPassword = "password123";
+        System.out.println("API Key: " + apiKey);
+        System.out.println("Database Password: " + dbPassword);
+    }
+}
